896f9511060cf94753dc7fc0a0bc355ddd240b3d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-25 20:30:00 +0300

    Updates

---
+++ precache-manifest.2a9dd070dcb6ad39acf2cb7f01225e7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fe77cf5021c47b6c064b257ce76479",
+    "url": "/mezidia-jobs/index.html"
+  },
+  {
+    "revision": "faaf5b1b16df95be8d1d",
+    "url": "/mezidia-jobs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d999f02006451faabf42",
+    "url": "/mezidia-jobs/static/js/2.54df3dbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mezidia-jobs/static/js/2.54df3dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faaf5b1b16df95be8d1d",
+    "url": "/mezidia-jobs/static/js/main.21e70151.chunk.js"
+  },
+  {
+    "revision": "0636e0b8e76089fab3e1",
+    "url": "/mezidia-jobs/static/js/runtime-main.f4d59753.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mezidia-jobs/static/media/logo.5d5d9eef.svg"
+  }
+]);
