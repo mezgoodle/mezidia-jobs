1843b264939dda23a88cc7c0dbaa3854a1fde89c
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-25 13:51:51 +0000

    Use reducer

--- src/components/useFetchJobs.js
+++ src/components/useFetchJobs.js
@@ -1,4 +1,31 @@
+import {useReducer} from 'react';
+
+const ACTIONS = {
+	MAKE_REQUEST: 'make-request',
+	GET_DATA: 'get-data',
+	ERROR: 'error',
+}
+
+function reducer(state, action) {
+	switch(action.type) {
+		case ACTIONS.MAKE_REQUEST:
+			return {loading: true, jobs: []}
+
+		case ACTIONS.GET_DATA:
+			return {...state, loading: false, jobs: action.payload.jobs}
+
+		case ACTIONS.ERROR:
+			return {...state, loading: false, error: action.payload.error, jobs: []}
+		default:
+			return state
+	}
+}
+
 export default function useFetchJobs(params, page) {
+	const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
+
+	
+
 	return {
 		jobs: [],
 		loading: true,
