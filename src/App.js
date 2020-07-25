5ece050d1ab86795a8305a1d88c83815611c0537
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-25 13:43:04 +0000

    Create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import useFetchJobs from './components/useFetchJobs';
+import {Container} from 'react-bootstrap';
 
 function App() {
+	const { jobs, loading, error } = useFetchJobs();
   return (
-    <div>
-      Hello Mezidia
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+			{error && <h1>Error. Try refresh it.</h1>}
+			<h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
