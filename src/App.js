b5ea53682e087b0f884b6360ae6774786a72eef6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-27 10:46:01 +0300

    Work with API

--- src/App.js
+++ src/App.js
@@ -4,27 +4,51 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { MDBContainer } from 'mdbreact';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <MDBContainer>
-        <div className="text-center">
-          <h1 className="h1">Mezidia jobs</h1>
-          <p>Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum.
-        Donec sed odio operae,
-        eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis
-        piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium
-        purus sit amet fermentum.</p>
-        </div>
-      </MDBContainer>
-      <MDBContainer>
-        
-      </MDBContainer>
-      <Table />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    jobs: [],
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            jobs: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <MDBContainer>
+          <div className="text-center">
+            <h1 className="h1">Mezidia jobs</h1>
+            <p>Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum.
+          Donec sed odio operae,
+          eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis
+          piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium
+          purus sit amet fermentum.</p>
+          </div>
+        </MDBContainer>
+        <MDBContainer>
+          
+        </MDBContainer>
+        <Table jobs={this.state}/>
+        <Footer />
+      </div>
+    )
+  };
 }
 
 export default App;
