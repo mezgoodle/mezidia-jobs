e98cf461980dc7bd28cf52858d06247dc2a600ad
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 18:25:57 +0300

    Install react-markdown

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Badge } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
   return (
@@ -15,6 +16,9 @@ export default function Job({ job }) {
             </Card.Subtitle>
             <Badge variant="secondary" className="mr-2">{job.type}</Badge>
             <Badge variant="secondary">{job.location}</Badge>
+            <div>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
           </div>
         </div>
       </Card.Body>
