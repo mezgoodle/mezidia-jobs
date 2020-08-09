771b8990de2d019dc0644668d86002cb8b7fa626
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 20:52:40 +0300

    Update SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Form, Col } from 'react-bootstrap';
 
 export default function SearchForm({ params, onParamChange }) {
   return (
-    <div>
-      hi
-    </div>
+    <Form className="mb-4">
+      <Form.Row>
+        <Form.Group as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
+        </Form.Group>
+      </Form.Row>
+    </Form>
   )
 }
