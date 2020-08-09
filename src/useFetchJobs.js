c743a1fd67e94693deec2d05cfddb8fc49b211a1
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 17:28:14 +0300

    Update useFetchJobs.js

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,7 +1,23 @@
 import { useReducer } from 'react';
 
-const reducer = (state, action) => {
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+}
 
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] }
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs }
+    case ACTIONS.ERROR:
+      return { ...state, loading: false, error: action.payload.jobs, jobs: [] }
+    default:
+      return state
+  }
 }
 
 export default function useFetchJobs(params, page) {
