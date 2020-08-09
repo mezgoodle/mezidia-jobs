97ed399b35b7b3903dfaff4352a98cb4d758fd46
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 21:03:46 +0300

    Update SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,7 +4,7 @@ import { Form, Col } from 'react-bootstrap';
 export default function SearchForm({ params, onParamChange }) {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
@@ -12,6 +12,9 @@ export default function SearchForm({ params, onParamChange }) {
         <Form.Group as={Col}>
           <Form.Label>Location</Form.Label>
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full_time" label="Only Full Time" type="checkbox" className="mb-2" />
         </Form.Group>
       </Form.Row>
     </Form>
