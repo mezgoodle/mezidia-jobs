be5c096fd6431d7526aa6e9acc488455f18cfd2d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 18:58:31 +0300

    Open/Hide details

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Card, Badge, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <Card>
       <Card.Body>
@@ -23,11 +25,17 @@ export default function Job({ job }) {
           <img className="d-none d-md-block" height="50" src={job.company_logo} alt={job.company} />
         </div>
         <Card.Text>
-          <Button variant="primary">View details</Button>
+          <Button 
+            onClick={() => setOpen(prevOpen => !prevOpen)} variant="primary"
+          >
+            View details
+          </Button>
         </Card.Text>
-        <div className="mt-4">
-          <ReactMarkdown source={job.description}/>
-        </div>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description}/>
+          </div>
+        </Collapse>
       </Card.Body>
     </Card>
   )
