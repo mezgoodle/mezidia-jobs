82ab6bfd94d2a54d55528a0237aafe6109e0a1c4
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 18:22:21 +0300

    Update Job component

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Card, Badge } from 'react-bootstrap';
 
 export default function Job({ job }) {
   return (
-    <div>
-      {job.title}
-    </div>
+    <Card>
+      <Card.Body>
+        <div className="d-flex justifu-content-between">
+          <div>
+            <Card.Title>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
   )
 }
