5913a7806a048f6c87c1cebe7e87e9a437cafab9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 17:23:57 +0300

    Update useFetchJobs.js

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,1 +1,16 @@
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+
+}
+
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+
   dispatch({ type: 'hello', payload: { x: 3}})
+  return {
+    jobs: [],
+    loading: false,
+    error: false,
+  }
+}
