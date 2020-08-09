06a380b7c51c8b22723d0c5bbd62df8f932657e3
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 19:02:08 +0300

    Some updates

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
   return (
     <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing.</h1>}
       {jobs.map(job => {
