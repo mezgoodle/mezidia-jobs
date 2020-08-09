94c18e8e242188654c9f33e3778eca3346a27823
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-09 17:37:18 +0300

    Work with token

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,5 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
 
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
@@ -6,6 +7,7 @@ const ACTIONS = {
   ERROR: 'error',
 }
 
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -23,10 +25,22 @@ const reducer = (state, action) => {
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
-  dispatch({ type: 'hello', payload: { x: 3}})
-  return {
-    jobs: [],
-    loading: false,
-    error: false,
-  }
+  useEffect(() => {
+    const cancelToken = axios.CancelToken.source();
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken.token,
+      params: { markdown: true, page: page, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+    }).catch(e => {
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+    })
+
+    return () => {
+      cancelToken.cancel()
+    }
+  }, [params, page])
+
+  return state
 }
