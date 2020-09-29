5320ee79e207601c5ab9354b4198581097032e10
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-29 18:22:11 +0300

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import useFetchJobs from './useFetchJobs';
 import Job from './components/Job';
 import JobsPagination from './components/JobsPagination';
 import SearchForm from './components/SearchForm';
+import NavbarComponent from './components/Navbar';
 
 const App = () => {
   const [params, setParams] = useState({});
@@ -20,17 +21,20 @@ const App = () => {
   }
 
   return (
-    <Container className="my-4">
-      <h1 className="mb-4">Mezidia Jobs</h1>
-      <SearchForm params={params} onParamChange={handleParamChange} />
-      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-      {loading && <h1>Loading...</h1>}
-      {error && <h1>Error. Try refreshing.</h1>}
-      {jobs.map(job => {
-        return <Job key={job.id} job={job} />
-      })}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-    </Container>
+    <div>
+      <NavbarComponent />
+      <Container className="my-4">    
+        <h1 className="mb-4">Mezidia Jobs</h1>
+        <SearchForm params={params} onParamChange={handleParamChange} />
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>Error. Try refreshing.</h1>}
+        {jobs.map(job => {
+          return <Job key={job.id} job={job} />
+        })}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      </Container>
+    </div>
   )
 }
 
