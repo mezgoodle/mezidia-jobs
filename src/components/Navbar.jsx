23fa528add812bb9d8ea549b3acfeb5cf5315a18
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-29 18:43:06 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import {Navbar, Nav }from 'react-bootstrap';
 export default function NavbarComponent() {
   return (
     <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="https://github.com/mezidia" target="_blank">Mezidia</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="https://github.com/mezidia/mezidia-jobs" target="_blank">Repository</Nav.Link>
+        <Nav.Link href="https://flask-jobs.herokuapp.com/" target="_blank" >Jobs</Nav.Link>
+        <Nav.Link href="mailto:mezgoodle@gmail.com">Propose a vacancy</Nav.Link>
       </Nav>
     </Navbar>
   )
